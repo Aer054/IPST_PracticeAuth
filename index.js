300c8ac755bca420121bbf9c81138261a67716c3
Aer054 <112958865+Aer054@users.noreply.github.com>
2024-07-10 12:05:24 +0700

    upd

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(errorHandler)
 const start = async()=>{
     try{
         await Sequelize.authenticate()
-        await Sequelize.sync({ force: true })
+        await Sequelize.sync()
         await Sequelize.query(`ALTER TABLE "UserTable" SET (autovacuum_enabled=true);`);
         app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
     }catch(e){
